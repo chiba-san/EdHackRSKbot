6867d50f623c3dccb71f7fd5fae716573d99f7d0
Alex Turyev <alex.turyev@breffi.ru>
2016-09-11 10:24:05 +0300

    fixes

--- Hackathon_Bot_Application_rsk_k161/Controllers/MessagesController.cs
+++ Hackathon_Bot_Application_rsk_k161/Controllers/MessagesController.cs
@@ -8,6 +8,7 @@ using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using DataBaseContext.DbModels;
 
 namespace Hackathon_Bot_Application_rsk_k161
 {
@@ -23,112 +24,161 @@ namespace Hackathon_Bot_Application_rsk_k161
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-
                 Activity typingAct = activity.CreateReply();
                 typingAct.Type = ActivityTypes.Typing;
                 await connector.Conversations.SendToConversationAsync(typingAct);
 
-                var handledComand = HandleBusinessLogicMessage(activity);
-                if (handledComand != null)
-                {
-                    await connector.Conversations.ReplyToActivityAsync(handledComand);
-                }
-                else
-                {
-                    Activity reply = activity.CreateReply($"");
+                Activity reply = activity.CreateReply($"");
 
-                    try
+                try
+                {
+                    using (DataBaseContext.Context db = new DataBaseContext.Context())
                     {
-                        using (DataBaseContext.Context db = new DataBaseContext.Context())
+                        var userName = activity.From.Name;
+                        var channel = activity.ChannelId;
+                        var user = db.CheckUser(userName, channel);
+
+                        if (activity.Text.Contains("cmd_"))
                         {
-                            var userName = activity.From.Name;
-                            var channel = activity.ChannelId;
+                            string[] data = activity.Text.Split('_');
+                            string command = data[1];
+                            string quizObj = data[2];
+                            int objId = int.Parse(data[3]);
 
-                            var user = db.CheckUser(userName, channel);
-                            bool isAdmin = db.IsAdmin(user);
-
-
-                            if (isAdmin)
+                            if (quizObj.Equals("quiz"))
                             {
-                                var onlineQuiz = db.GetOnlineQuiz(user);
-                                if (onlineQuiz != null)
+                                var onlineQuize = db.Quizzes.FirstOrDefault(t => t.Id == objId);
+                                if (onlineQuize != null)
                                 {
-                                    var activeQuiestoin = db.GetActiveQuestion(onlineQuiz);
-                                    if (activeQuiestoin != null)
-                                    {
-                                        //Promt admin to "Next Question" or "Stop"
-                                    }
-                                    else
-                                    {
-                                        //Promt admin to "Next Question" or "Finish"
-                                    }
+                                    onlineQuize.Status = (command.Equals("start")) ? 2 : 1;
+                                    db.SaveChanges();
+                                    reply = activity.CreateReply($"Опрос \"{onlineQuize.Name}\" {(onlineQuize.Status == 2 ? "запущен" : "остановлен")}  в {DateTime.Now.ToShortTimeString()}");
+                                    await connector.Conversations.SendToConversationAsync(reply);
                                 }
-                                else
+                            }
+                            else if (quizObj.Equals("question"))
+                            {
+                                var activeQuestion = db.Questions.FirstOrDefault(t => t.Id == objId);
+                                if (activeQuestion != null)
                                 {
-
-                                    //Display list and promt user to select
-                                    reply.Recipient = activity.From;
-                                    reply.Type = "message";
-                                    reply.Attachments = new List<Attachment>();
-
-                                    List<CardAction> cardButtons = new List<CardAction>();
-                                    var availableQuizzes = db.GetAvailableQuizzes(user);
-                                    foreach (var quiz in availableQuizzes)
+                                    if (command.Equals("stop"))
                                     {
-                                        CardAction plButton = new CardAction()
+                                        StateClient stateClient = activity.GetStateClient();
+                                        BotData userData = stateClient.BotState.GetUserData(activity.ChannelId, activity.From.Id);
+                                        userData.SetProperty<string>("questionId", objId.ToString());
+                                        stateClient.BotState.SetUserData(activity.ChannelId, activity.From.Id, userData);
+                                        reply = activity.CreateReply($"Вопрос \"{activeQuestion.Text}\" остановлен  в {DateTime.Now.ToShortTimeString()}");
+                                        await connector.Conversations.ReplyToActivityAsync(reply);
+
+                                        //Promt admin to "Next Question" or "Finish
+                                        var onlineQuiz = db.GetOnlineQuiz(user);
+                                        if (onlineQuiz != null)
                                         {
-                                            Value = "cmd_start_quiz_" + quiz.Id.ToString(),
-                                            Type = "postBack",
-                                            Title = quiz.Name
-                                        };
-                                        cardButtons.Add(plButton);
+                                            reply = PromtUserForNextQuestionOrFinishQuiz(activity, onlineQuiz);
+                                            await connector.Conversations.ReplyToActivityAsync(reply);
+                                        }
                                     }
-                                    HeroCard plCard = new HeroCard()
+                                    else if (command.Equals("start"))
                                     {
-                                        Title = "Доступные опросы",
-                                        Subtitle = "Выберите один",
-                                        Buttons = cardButtons
-                                    };
-                                    Attachment plAttachment = plCard.ToAttachment();
-                                    reply.Attachments.Add(plAttachment);
+                                        var question = db.SetActiveNextQuestion(activeQuestion);
+                                        reply = activity.CreateReply($"Вопрос \"{question.Text}\" запущен в {DateTime.Now.ToShortTimeString()}");
+
+                                        //Promt admin to "Next Question" or "Stop"                                 
+                                        reply = PromtUserForNextQuestionOrStopQuiz(activity, activeQuestion);
+                                        await connector.Conversations.ReplyToActivityAsync(reply);
+                                    }
                                 }
                             }
                             else
                             {
-                                var onlineQuiz = db.GetOnlineQuiz(user);
-                                if (onlineQuiz != null)
+
+                                bool isAdmin = db.IsAdmin(user);
+
+                                if (isAdmin)
                                 {
-                                    var activeQuestion = db.GetActiveQuestion(onlineQuiz);
-                                    if (activeQuestion != null)
+                                    var onlineQuiz = db.GetOnlineQuiz(user);
+                                    if (onlineQuiz != null)
                                     {
-                                        //Check user state for answer 
-                                        bool alreadyAnswered = false;
-                                        if (alreadyAnswered)
+
+                                        var activeQuiestoin = db.GetActiveQuestion(onlineQuiz);
+                                        if (activeQuiestoin != null)
                                         {
-                                            activity.CreateReply($"Вы уже ответили на данный вопрос!");
+                                            //Promt admin to "Next Question" or "Stop"                                                                                                                   
+                                            reply = PromtUserForNextQuestionOrStopQuiz(activity, activeQuiestoin);
                                         }
                                         else
                                         {
-                                            //Promt Question with Options and wait for user responses                                        
+                                            //Promt admin to "Next Question" or "Finish"
+                                            reply = PromtUserForNextQuestionOrFinishQuiz(activity, onlineQuiz);
                                         }
+                                        await connector.Conversations.SendToConversationAsync(reply);
+
                                     }
                                     else
                                     {
-                                        activity.CreateReply($"Ждем следующий вопрос!");
+                                        //Display list and promt user to select
+                                        reply.Recipient = activity.From;
+                                        reply.Type = "message";
+                                        reply.Attachments = new List<Attachment>();
+
+                                        List<CardAction> cardButtons = new List<CardAction>();
+                                        var availableQuizzes = db.GetAvailableQuizzes(user);
+                                        foreach (var quiz in availableQuizzes)
+                                        {
+                                            CardAction plButton = new CardAction()
+                                            {
+                                                Value = "cmd_start_quiz_" + quiz.Id.ToString(),
+                                                Type = "postBack",
+                                                Title = quiz.Name
+                                            };
+                                            cardButtons.Add(plButton);
+                                        }
+                                        HeroCard plCard = new HeroCard()
+                                        {
+                                            Title = "Доступные опросы",
+                                            Buttons = cardButtons
+                                        };
+                                        Attachment plAttachment = plCard.ToAttachment();
+                                        reply.Attachments.Add(plAttachment);
+                                        await connector.Conversations.SendToConversationAsync(reply);
                                     }
                                 }
                                 else
                                 {
-                                    activity.CreateReply($"!");
+                                    var onlineQuiz = db.GetOnlineQuiz(user);
+                                    if (onlineQuiz != null)
+                                    {
+                                        var activeQuestion = db.GetActiveQuestion(onlineQuiz);
+                                        if (activeQuestion != null)
+                                        {
+                                            //Check user state for answer 
+                                            bool alreadyAnswered = false;
+                                            if (alreadyAnswered)
+                                            {
+                                                activity.CreateReply($"Вы уже ответили на данный вопрос!");
+                                            }
+                                            else
+                                            {
+                                                //Promt Question with Options and wait for user responses                                        
+                                            }
+                                        }
+                                        else
+                                        {
+                                            activity.CreateReply($"Ждем следующий вопрос!");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        activity.CreateReply($"!");
+                                    }
                                 }
                             }
                         }
                     }
-                    catch (Exception ex)
-                    {
-                        reply = activity.CreateReply($"{ex.Message}");
-                    }
-                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                catch (Exception ex)
+                {
+                    reply = activity.CreateReply($"{ex.Message}");
                 }
             }
             else
@@ -139,43 +189,82 @@ namespace Hackathon_Bot_Application_rsk_k161
             return response;
         }
 
-        private Activity HandleBusinessLogicMessage(Activity message)
+        private static Activity PromtUserForNextQuestionOrStopQuiz(Activity activity, Question activeQuiestoin)
         {
-            if (message.Text.Contains("cmd_"))
-            {
-                try
-                {
-                    using (DataBaseContext.Context db = new DataBaseContext.Context())
-                    {
-                        message = message.CreateReply($"Выполнено!");
-                        string[] data = message.Text.Split('_');
-                        string command = data[1];
-                        string quizObj = data[2];
-                        string objId = data[3];
+            Activity reply = activity.CreateReply($"");
+            reply.Recipient = activity.From;
+            reply.Type = "message";
+            reply.Attachments = new List<Attachment>();
 
-                        if (quizObj.Equals("quiz"))
-                        {
-                            var onlineQuize = db.Quizzes.FirstOrDefault(t => t.Id == int.Parse(objId));
-                            if (onlineQuize != null)
-                            {
-                                onlineQuize.Status = (command.Equals("start"))?2:1;
-                                db.SaveChanges();
-                                message = message.CreateReply($"Опрос \"{onlineQuize.Name}\" {(onlineQuize.Status==2? "запущен":"остановлен")}  в {DateTime.Now.ToShortTimeString()}");
-                            }
-                        }
-                        return message;
-                                                
-                    }
-                }
-                catch (Exception ex)
-                {
-                    message = message.CreateReply($"{ex.Message}");
-                    return message;
-                }
-            }
-            else
-                return null;
+            List<CardAction> cardButtons = new List<CardAction>();
+            CardAction plButtonNextQuest = new CardAction()
+            {
+                Value = "cmd_start_question_" + activeQuiestoin.Id.ToString(),
+                Type = "postBack",
+                Title = "Cледующий"
+            };
+            cardButtons.Add(plButtonNextQuest);
+
+            CardAction plButtonStop = new CardAction()
+            {
+                Value = "cmd_stop_question_" + activeQuiestoin.Id.ToString(),
+                Type = "postBack",
+                Title = "Стоп"
+            };
+            cardButtons.Add(plButtonStop);
+
+            HeroCard plCard = new HeroCard()
+            {
+                Title = "Вы можете:",
+                Buttons = cardButtons
+            };
+            Attachment plAttachment = plCard.ToAttachment();
+            reply.Attachments.Add(plAttachment);
+            return reply;
         }
+
+        private static Activity PromtUserForNextQuestionOrFinishQuiz(Activity activity, Quiz current)
+        {
+            Activity reply = activity.CreateReply($"");
+            reply.Recipient = activity.From;
+            reply.Type = "message";
+            reply.Attachments = new List<Attachment>();
+
+            List<CardAction> cardButtons = new List<CardAction>();
+            StateClient stateClient = activity.GetStateClient();
+            BotData userData = stateClient.BotState.GetUserData(activity.ChannelId, activity.From.Id);
+            var lastActiveQuestionId = userData.GetProperty<string>("questionId");
+            //FIND activequestion from previous state
+            if (lastActiveQuestionId != null)
+            {
+                CardAction plButtonNextQuest = new CardAction()
+                {
+
+                    Value = "cmd_start_question_" + lastActiveQuestionId,
+                    Type = "postBack",
+                    Title = "Следующий"
+                };
+                cardButtons.Add(plButtonNextQuest);
+            }
+
+            CardAction plButtonStop = new CardAction()
+            {
+                Value = "cmd_stop_quiz_" + current.Id.ToString(),
+                Type = "postBack",
+                Title = "Закончить"
+            };
+            cardButtons.Add(plButtonStop);
+
+            HeroCard plCard = new HeroCard()
+            {
+                Title = "Вы можете:",
+                Buttons = cardButtons
+            };
+            Attachment plAttachment = plCard.ToAttachment();
+            reply.Attachments.Add(plAttachment);
+            return reply;
+        }
+
 
         private Activity HandleSystemMessage(Activity message)
         {
